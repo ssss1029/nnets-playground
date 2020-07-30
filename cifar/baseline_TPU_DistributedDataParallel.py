17950571d90a1700222a40255b74cf841aec187d
Saurav Kadavath <sauravkadavath@berkeley.edu>
2020-07-30 11:44:59 -0700

    Distributed kind of works

--- cifar/baseline_TPU_DistributedDataParallel.py
+++ cifar/baseline_TPU_DistributedDataParallel.py
@@ -209,8 +209,9 @@ def main(index, args):
 
         # Spawn a bunch of processes, one for each TPU core.
         train_loss = train(train_loader, net, optimizer, scheduler, xla_device, args)
-        xm.rendezvous("calc_train_loss")
-        train_loss = xm.mesh_reduce("calc_train_loss", train_loss, lambda _lst: sum(_lst) / len(_lst))
+        # TODO: Make this work.
+        # xm.rendezvous("calc_train_loss")
+        # train_loss = xm.mesh_reduce("calc_train_loss", train_loss, lambda _lst: sum(_lst) / len(_lst))
         state['train_loss'] = train_loss
 
         # test()
