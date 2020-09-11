ed840fe9dd4851d31585bb29f00deab5ae570885
Saurav Kadavath <sauravkadavath@berkeley.edu>
2020-09-10 22:10:26 -0700

    imcrement

--- imagenet/train_imagenet_distorted.py
+++ imagenet/train_imagenet_distorted.py
@@ -143,35 +143,8 @@ def main_worker(index, ngpus_per_node, args):
     else:
         print("=> creating pre-trained model (pretrained is forced) '{}'".format(args.arch))
         model = models.__dict__[args.arch](pretrained=True)
-
-    # if args.distributed:
-    #     # For multiprocessing distributed, DistributedDataParallel constructor
-    #     # should always set the single device scope, otherwise,
-    #     # DistributedDataParallel will use all available devices.
-    #     if args.gpu is not None:
-    #         torch.cuda.set_device(args.gpu)
-    #         model.cuda(args.gpu)
-    #         # When using a single GPU per process and per
-    #         # DistributedDataParallel, we need to divide the batch size
-    #         # ourselves based on the total number of GPUs we have
-    #         args.batch_size = int(args.batch_size / ngpus_per_node)
-    #         args.workers = int((args.workers + ngpus_per_node - 1) / ngpus_per_node)
-    #         model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
-    #     else:
-    #         model.cuda()
-    #         # DistributedDataParallel will divide and allocate batch_size to all
-    #         # available GPUs if device_ids are not set
-    #         model = torch.nn.parallel.DistributedDataParallel(model)
-    # elif args.gpu is not None:
-    #     torch.cuda.set_device(args.gpu)
-    #     model = model.cuda(args.gpu)
-    # else:
-    #     # DataParallel will divide and allocate batch_size to all available GPUs
-    #     if args.arch.startswith('alexnet') or args.arch.startswith('vgg'):
-    #         model.features = torch.nn.DataParallel(model.features)
-    #         model.cuda()
-    #     else:
-    #         model = torch.nn.DataParallel(model).cuda()
+    
+    model = model.to(DEVICE)
 
     optimizer = torch.optim.SGD(model.parameters(), args.lr,
                                 momentum=args.momentum,
@@ -328,15 +301,15 @@ def main_worker(index, ngpus_per_node, args):
 
 
 def train(train_loader, model, optimizer, scheduler, epoch, args, DEVICE):    
-    batch_time = AverageMeter('Time', ':6.3f')
-    data_time = AverageMeter('Data', ':6.3f')
-    losses = AverageMeter('Loss', ':.4e')
-    top1 = AverageMeter('Acc@1', ':6.2f')
-    top5 = AverageMeter('Acc@5', ':6.2f')
-    progress = ProgressMeter(
-        len(train_loader),
-        [batch_time, data_time, losses, top1, top5],
-        prefix="Epoch: [{}]".format(epoch))
+    # batch_time = AverageMeter('Time', ':6.3f')
+    # data_time = AverageMeter('Data', ':6.3f')
+    # losses = AverageMeter('Loss', ':.4e')
+    # top1 = AverageMeter('Acc@1', ':6.2f')
+    # top5 = AverageMeter('Acc@5', ':6.2f')
+    # progress = ProgressMeter(
+    #     len(train_loader),
+    #     [batch_time, data_time, losses, top1, top5],
+    #     prefix="Epoch: [{}]".format(epoch))
 
     # switch to train mode
     model = model.train().to(DEVICE)
@@ -347,13 +320,15 @@ def train(train_loader, model, optimizer, scheduler, epoch, args, DEVICE):
         loader = train_loader
 
     end = time.time()
-    for i, (images, target) in enumerate(train_loader):
+    for i, (images, target) in enumerate(loader):
         # measure data loading time
         data_time.update(time.time() - end)
 
+        bx = images
+        by = target
 
-        bx = images.to(DEVICE)
-        by = target.to(DEVICE)
+        print("Zero grad")
+        optimizer.zero_grad()
 
         print("Forward")
         logits = model(bx)
@@ -368,18 +343,11 @@ def train(train_loader, model, optimizer, scheduler, epoch, args, DEVICE):
         # top1.update(acc1[0], images.size(0))
         # top5.update(acc5[0], images.size(0))
 
-        print("Zero grad")
-        optimizer.zero_grad()
-
         print("Backward")
         loss.backward()
 
         print("Step")
-        # compute gradient and do SGD step
-        if args.device == 'tpu':
-            xm.optimizer_step(optimizer)
-        else:
-            optimizer.step()
+        xm.optimizer_step(optimizer)
         
         print("Scheduler step")
         scheduler.step()
