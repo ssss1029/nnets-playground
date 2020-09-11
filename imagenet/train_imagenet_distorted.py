c7cc5018dd41a28a2c02abbd5b8d46a40b16799d
Saurav Kadavath <sauravkadavath@berkeley.edu>
2020-09-10 22:12:05 -0700

    imcrement

--- imagenet/train_imagenet_distorted.py
+++ imagenet/train_imagenet_distorted.py
@@ -301,15 +301,15 @@ def main_worker(index, ngpus_per_node, args):
 
 
 def train(train_loader, model, optimizer, scheduler, epoch, args, DEVICE):    
-    # batch_time = AverageMeter('Time', ':6.3f')
-    # data_time = AverageMeter('Data', ':6.3f')
-    # losses = AverageMeter('Loss', ':.4e')
-    # top1 = AverageMeter('Acc@1', ':6.2f')
-    # top5 = AverageMeter('Acc@5', ':6.2f')
-    # progress = ProgressMeter(
-    #     len(train_loader),
-    #     [batch_time, data_time, losses, top1, top5],
-    #     prefix="Epoch: [{}]".format(epoch))
+    batch_time = AverageMeter('Time', ':6.3f')
+    data_time = AverageMeter('Data', ':6.3f')
+    losses = AverageMeter('Loss', ':.4e')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
+    progress = ProgressMeter(
+        len(train_loader),
+        [batch_time, data_time, losses, top1, top5],
+        prefix="Epoch: [{}]".format(epoch))
 
     # switch to train mode
     model = model.train().to(DEVICE)
